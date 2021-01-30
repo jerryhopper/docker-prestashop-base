104a1c9773f0ed4997c0f410116b688574c5ff2a
JerryHopper <hopper.jerry@gmail.com>
2021-01-31 00:13:34 +0100

    Update prestashop.sh

--- app/prestashop.sh
+++ app/prestashop.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
+THEFILE=prestashop_1.7.7.1.zip
+DOWNLOADFILE=https://download.prestashop.com/download/releases/$THEFILE
+
+CONTAINER=presta_beststout_shop
+
+
+fixPermissions(){
+  echo "Fix permissions"
+  docker exec $CONTAINER bash -c "chmod +w ./var/cache && chmod +w ./var/logs && chmod +w ./img && chmod +w ./mails && chmod +w ./modules && chmod +w ./translations && chmod +w ./upload && chmod +w ./download && chmod +w ./app/config && chmod +w ./app/Resources/tramslations"
+}
+
+fixOwnership(){
+  echo "Fix Ownership"
+  docker exec $CONTAINER bash -c "chown -R www-data:www-data /app"  
+}
 
 
 
 if [ "$1"="install" ];then
-  wget https://github.com/PrestaShop/PrestaShop/archive/1.7.7.1.zip
+  docker exec $CONTAINER bash -c "cd /app/web && wget $DOWNLOADFILE && unzip $THEFILE && rm -f $THEFILE && unzip prestashop.zip" 
+  fixPermissions
+  fixOwnership
   exit 0
 fi
 
 
 if [ "$1"="fixowner" ];then
   # Set ownership
-  echo "Fix Ownership"
-  docker exec presta_beststout_shop bash -c "chown -R www-data:www-data /app"  
+  fixOwnership
   exit 0
 fi
 
 
 if [ "$1"="fixpermissions" ];then
   # Set permissions
-  echo "Fix permissions"
-  docker exec presta_beststout_shop bash -c "chmod +w ./var/cache && chmod +w ./var/logs && chmod +w ./img && chmod +w ./mails && chmod +w ./modules && chmod +w ./translations && chmod +w ./upload && chmod +w ./download && chmod +w ./app/config && chmod +w ./app/Resources/tramslations"
+  fixPermissions
   exit 0
 fi
 
