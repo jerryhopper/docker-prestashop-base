c1e9b792db4de511c9d5d3672a7f6356caa37139
JerryHopper <hopper.jerry@gmail.com>
2021-01-30 17:34:26 +0100

    Update backupVolume.sh

--- app/backupVolume.sh
+++ app/backupVolume.sh
@@ -5,10 +5,20 @@ THEDATE="$(date +%Y%m%d)"
 VOLUME=$1
 FILENAME=$2
 
+if [ "$1" = "" ];then
+  echo "Missing 1st paramater (volume)"
+  exit
+fi
+if [ "$2" = "" ];then
+  echo "Missing 2nd paramater (filename)"
+  FILENAME=$1
+  exit
+fi
+
 if [ ! -d $PWD/backup ];then
   mkdir ./backup
 fi
 
 
 # Backup the www directory in a tar
-docker run --rm -v $VOLUME:/app -v $PWD/backup:/backup ubuntu bash -c "cd /app && tar cvf ./$2.tar ." > /dev/null
+docker run --rm -v $VOLUME:/app -v $PWD/backup:/backup ubuntu bash -c "cd /app && tar cvf ./$FILENAME.tar ." > /dev/null
