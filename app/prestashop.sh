eea0683351ebe164354f05e03ee56c234ed87872
JerryHopper <hopper.jerry@gmail.com>
2021-01-30 20:18:20 +0100

    Update prestashop.sh

--- app/prestashop.sh
+++ app/prestashop.sh
@@ -3,17 +3,15 @@
 
 
 
-docker exec -it  
-chown -R www-data:www-data ./
+# Set ownership
+echo "Fix Ownership"
+docker exec presta_beststout_shop bash -c "chown -R www-data:www-data /app"
 
-chmod +w ./var/cache
-chmod +w ./var/logs
-chmod +w ./img
-chmod +w ./mails
-chmod +w ./modules
-chmod +w ./translations
-chmod +w ./upload
-chmod +w ./download
-chmod +w ./app/config
-chmod +w ./app/Resources/tramslations
+
+
+# Set permissions
+echo "Fix permissions"
+docker exec presta_beststout_shop bash -c "chmod +w ./var/cache && chmod +w ./var/logs && chmod +w ./img && chmod +w ./mails && chmod +w ./modules && chmod +w ./translations && chmod +w ./upload && chmod +w ./download && chmod +w ./app/config && chmod +w ./app/Resources/tramslations"
+
+
 
