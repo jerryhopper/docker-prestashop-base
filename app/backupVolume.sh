52dfd54241353f3e6fdc25a6df2519f2b6260969
JerryHopper <hopper.jerry@gmail.com>
2021-01-31 00:25:01 +0100

    Update backupVolume.sh

--- app/backupVolume.sh
+++ app/backupVolume.sh
@@ -5,6 +5,21 @@ THEDATE="$(date +%Y%m%d)"
 VOLUME=$1
 FILENAME=$2
 
+
+
+function backupDB(){
+  #docker run --rm -v $VOLUME:/app -v $PWD/backup:/backup ubuntu bash -c "ls -latr /"
+  #docker run --rm -v $VOLUME:/app -v $PWD/backup:/backup ubuntu bash -c "ls -latr /app"  
+  
+  # Backup the www directory in a tar
+  docker run --rm -v $VOLUME:/app -v $PWD/backup:/backup ubuntu bash -c "cd /app && tar cvf /backup/$FILENAME.tar ." > /dev/null
+
+}
+
+if [ ! -d $PWD/backup ];then
+  mkdir ./backup
+fi
+
 if [ "$1" = "" ];then
   echo "Missing 1st paramater (volume)"
   exit
@@ -12,16 +27,7 @@ fi
 if [ "$2" = "" ];then
   echo "Missing 2nd paramater (filename), using volumename"
   FILENAME=$1
-  
+  backupDB
 fi
 
-if [ ! -d $PWD/backup ];then
-  mkdir ./backup
-fi
 
-#docker run --rm -v $VOLUME:/app -v $PWD/backup:/backup ubuntu bash -c "ls -latr /"
-
-#docker run --rm -v $VOLUME:/app -v $PWD/backup:/backup ubuntu bash -c "ls -latr /app"
-
-# Backup the www directory in a tar
-docker run --rm -v $VOLUME:/app -v $PWD/backup:/backup ubuntu bash -c "cd /app && tar cvf /backup/$FILENAME.tar ." > /dev/null
