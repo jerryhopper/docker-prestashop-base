252e039f56892d50afa47180a5deaaa8a0d63aad
JerryHopper <hopper.jerry@gmail.com>
2021-01-30 17:25:10 +0100

    Update backupVolume.sh

--- app/backupVolume.sh
+++ app/backupVolume.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+THEDATE="$(date +%Y%m%d)"
+
+VOLUME=$1
+FILENAME=$2
+
+
+# Backup the www directory in a tar
+docker run --rm --v /app:$VOLUME -v $PWD/backup:/backup ubuntu bash -c "cd /app && tar cvf ./$2.tar ." > /dev/null
